9eae95b75cb99378e2d16e43b6ed446f0d31c263
cod3kid <djsufail@gmail.com>
2024-07-04 00:10:40 +0530

    Link element in Remix

--- starter/app/routes/index.jsx
+++ starter/app/routes/index.jsx
@@ -1,9 +1,10 @@
-import { Link } from '@remix-run/react';
+import { Link } from "@remix-run/react";
 
 export default function Index() {
   return (
     <>
       <h1>Hello World!</h1>
+      <Link to="/demo">Go to Demo</Link>
     </>
   );
 }
