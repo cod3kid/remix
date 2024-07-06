c3564b7d25091dadc7edca0a3e698206a82ed9a7
cod3kid <djsufail@gmail.com>
2024-07-07 02:52:56 +0530

    action function to execute server side code

--- starter/app/routes/notes.jsx
+++ starter/app/routes/notes.jsx
@@ -1,4 +1,6 @@
+import { redirect } from "@remix-run/node";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
+import { getStoredNotes, storeNotes } from "~/data/notes";
 
 function NotesPage() {
   return (
@@ -8,8 +10,23 @@ function NotesPage() {
   );
 }
 
-export default NotesPage;
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const noteData = {
+    id: new Date().toISOString(),
+    title: formData.get("title"),
+    content: formData.get("content"),
+  };
+
+  const existingNotes = await getStoredNotes();
+  const updatedNotes = [...existingNotes, noteData];
+  await storeNotes(updatedNotes);
+
+  return redirect("/notes");
+};
 
 export const links = () => {
   return [...newNoteLinks()];
 };
+
+export default NotesPage;
