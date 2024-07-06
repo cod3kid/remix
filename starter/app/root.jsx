0ce1ede90354fbcd7c53a764d5905dfcd23988f7
cod3kid <djsufail@gmail.com>
2024-07-07 02:23:02 +0530

    Header component with NavLink

--- starter/app/root.jsx
+++ starter/app/root.jsx
@@ -7,6 +7,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import styles from "~/styles/main.css";
+import MainNavigation from "./components/MainNavigation";
 
 export const meta = () => ({
   charset: "utf-8",
@@ -22,6 +23,9 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <header>
+          <MainNavigation />
+        </header>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
