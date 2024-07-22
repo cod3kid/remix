6d0e699b6131f51c0eb1495804bf8e6398496004
cod3kid <djsufail@gmail.com>
2024-07-22 18:02:55 +0530

    Expenses App - Programmatic Navigation

--- expense-app/app/routes/expenses/$id.jsx
+++ expense-app/app/routes/expenses/$id.jsx
@@ -1,11 +1,18 @@
 // /expenses/<some-id> => /expenses/expense-1, /expenses/e-1
 
+import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
 
 export default function UpdateExpensesPage() {
+  const navigate = useNavigate();
+
+  const handleCloseHandler = () => {
+    navigate("..");
+  };
+
   return (
-    <Modal>
+    <Modal onClose={handleCloseHandler}>
       <ExpenseForm />
     </Modal>
   );
--- expense-app/app/routes/expenses/add.jsx
+++ expense-app/app/routes/expenses/add.jsx
@@ -1,11 +1,18 @@
 // /expenses/add
 
+import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
 
 export default function AddExpensesPage() {
+  const navigate = useNavigate();
+
+  const handleCloseHandler = () => {
+    navigate("..");
+  };
+
   return (
-    <Modal>
+    <Modal onClose={handleCloseHandler}>
       <ExpenseForm />
     </Modal>
   );
