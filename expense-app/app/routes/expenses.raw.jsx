1d2cbf66c6d9a5d26041921210414d2dd7e3116f
cod3kid <djsufail@gmail.com>
2024-07-25 21:02:33 +0530

    Expenses App - Resource Route

--- expense-app/app/routes/expenses.raw.jsx
+++ expense-app/app/routes/expenses.raw.jsx
@@ -1,5 +1,20 @@
 // /expenses/raw
 
-export default function ExpensesRawPage() {
-  return <h1>Expenses Raw Page</h1>;
-}
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Buy a Book",
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "Gym Membership",
+    amount: 8.99,
+    date: new Date().toISOString(),
+  },
+];
+
+export const loader = () => {
+  return DUMMY_EXPENSES;
+};
