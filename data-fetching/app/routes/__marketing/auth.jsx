854c99137d2910a677a029a4ef2c125dfd436e54
cod3kid <djsufail@gmail.com>
2024-08-06 19:40:10 +0530

    Data Fetching - Extracting search params from the backend

--- data-fetching/app/routes/__marketing/auth.jsx
+++ data-fetching/app/routes/__marketing/auth.jsx
@@ -1,10 +1,24 @@
-import AuthForm from '~/components/auth/AuthForm';
-import authStyles from '~/styles/auth.css';
+import AuthForm from "~/components/auth/AuthForm";
+import authStyles from "~/styles/auth.css";
 
 export default function AuthPage() {
   return <AuthForm />;
 }
 
 export function links() {
-  return [{ rel: 'stylesheet', href: authStyles }];
+  return [{ rel: "stylesheet", href: authStyles }];
 }
+
+export const action = async ({ request }) => {
+  const searchParams = URL(request.url);
+  const authMode = searchParams.authMode || "login";
+
+  const formData = await request.formData();
+  const credentials = Object.fromEntries(formData);
+
+  if (authMode === "login") {
+    // Login logic
+  } else {
+    // Sign up logic
+  }
+};
