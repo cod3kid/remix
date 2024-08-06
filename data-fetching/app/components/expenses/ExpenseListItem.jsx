7aec0c4a48809691ebdbdc814f28db36e201123a
cod3kid <djsufail@gmail.com>
2024-08-06 19:30:16 +0530

    Data Fetching - Programmatic delete using useFetcher hook and submit method

--- data-fetching/app/components/expenses/ExpenseListItem.jsx
+++ data-fetching/app/components/expenses/ExpenseListItem.jsx
@@ -1,8 +1,21 @@
-import { Form, Link } from "@remix-run/react";
+import { Form, Link, useFetcher } from "@remix-run/react";
 
 function ExpenseListItem({ id, title, amount }) {
+  const fetcher = useFetcher();
   function deleteExpenseItemHandler() {
     // tbd
+    const response = confirm("Do you really want to delete?");
+    if (!response) return;
+
+    fetcher.submit(null, { method: "delete", action: `/expenses/${id}` });
+  }
+
+  if (fetcher.state !== "idle") {
+    return (
+      <article className="expense-item locked">
+        <p> Deleteing...</p>
+      </article>
+    );
   }
 
   return (
@@ -12,10 +25,10 @@ function ExpenseListItem({ id, title, amount }) {
         <p className="expense-amount">${amount.toFixed(2)}</p>
       </div>
       <menu className="expense-actions">
-        {/* <button onClick={deleteExpenseItemHandler}>Delete</button> */}
-        <Form method="delete" action={`/expenses/${id}`}>
+        <button onClick={deleteExpenseItemHandler}>Delete</button>
+        {/* <Form method="delete" action={`/expenses/${id}`}>
           <button>Delete</button>
-        </Form>
+        </Form> */}
         <Link to={id}>Edit</Link>
       </menu>
     </article>
