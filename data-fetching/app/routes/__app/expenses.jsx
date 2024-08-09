ea72c3de9ece39a51a8157dae4b11c3f1ce3270d
cod3kid <djsufail@gmail.com>
2024-08-09 22:25:58 +0530

    Data Fetching - Handling Error without Error or Catch Boundary

--- data-fetching/app/routes/__app/expenses.jsx
+++ data-fetching/app/routes/__app/expenses.jsx
@@ -1,17 +1,34 @@
 // /expenses => shared layout
 
-import { Outlet, useLoaderData } from "@remix-run/react";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { FaDownload, FaPlus } from "react-icons/fa";
 import ExpensesList from "~/components/expenses/ExpensesList";
 import { getExpenses } from "~/data/expenses.server";
 
 export default function ExpensesLayout() {
   const expenses = useLoaderData();
+  const hasExpenses = expenses && expenses.length > 0;
 
   return (
     <>
       <Outlet />
       <main>
-        <ExpensesList expenses={expenses} />
+        <section id="expenses-actions">
+          <Link to="add">
+            <FaPlus />
+            <span>Add Expense</span>
+          </Link>
+          <a href="/expenses/raw">
+            <FaDownload />
+            <span>Load Raw Data</span>
+          </a>
+        </section>
+        {hasExpenses && <ExpensesList expenses={expenses} />}
+        {!hasExpenses && (
+          <section id="no-expenses">
+            <h1>No expenses found</h1>
+          </section>
+        )}
       </main>
     </>
   );
