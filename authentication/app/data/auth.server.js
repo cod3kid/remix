5b032d160307a648fa5e68cf96cf10a8558cbeec
cod3kid <djsufail@gmail.com>
2024-08-14 01:41:50 +0530

    Authentication - Login function - Part 2 - Session Storage Preparation

--- authentication/app/data/auth.server.js
+++ authentication/app/data/auth.server.js
@@ -1,5 +1,17 @@
 import { prisma } from "./database.server";
 import { compare, hash } from "bcryptjs";
+import { createCookieSessionStorage } from "@remix-run/node";
+
+const SESSION_SECRET = process.env.SESSION_SECRET;
+const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    secure: process.env.NODE_ENV === "production",
+    secrets: [SESSION_SECRET],
+    sameSite: "lax",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    httpOnly: true,
+  },
+});
 
 export async function signUp({ email, password }) {
   const existingUser = await prisma.user.findFirst({ where: { email } });
